6425a783088941df26fc6f6a15cd42e5ce452a38
neethi-13 <neethikrishnan.v@gmail.com>
2025-08-08 17:13:05 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,144 @@
+
+import './App.css'
+import { useState , useEffect } from 'react';
+import axios from 'axios';
+function App() {
+  const [user , setUser] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [add , setAdd] = useState({name:"" , age:"",city :""});
+  const [model , setModel] = useState(false);
+  const getAlluser = async () => {
+    await axios.get("http://localhost:8000/users").then((res)=>{
+      setUser(res.data);
+      
+      setSearch(res.data);
+    })
+  }
+
+  // Searching the Data
+
+  const changes = (e) =>{
+    const text = e.target.value.toLowerCase();
+    const searchs = user.filter((user) =>user.name.toLowerCase().includes(text) || user.city.toLowerCase().includes(text));
+      setSearch(searchs);
+  
+  } 
+// Adding the Record
+  const AddRecordbtn =  ()=>{
+    setAdd({name:"" , age:"", city:""}); 
+    setModel(true);
+  };
+   // clse the modal
+   const close = ()=>{
+    setModel(false);
+    getAlluser();
+   }
+   // Add the Record
+   const handleData = (e) =>{
+    setAdd({...add,[e.target.name] : e.target.value});
+   }
+   //ADD USER
+   const adduser = async (e)=>{
+    e.preventDefault();
+    if (!add.name.trim() || !add.age.trim() || !add.city.trim()) {
+        alert("All the fields are required");
+        return;
+      }
+    if(add.id){
+      await axios.patch(`http://localhost:8000/users/${add.id}`,add).then((res)=>{
+        alert(res.data.message);
+        close();
+      })
+    }else{
+      
+      await axios.post("http://localhost:8000/users",add).then((res)=>{
+        
+        alert(res.data.message);
+        
+      })
+      close();
+    }
+   }
+   //Update User
+
+   const updateDetail = (us)=>{
+    setAdd(us);
+    setModel(true);
+
+   }
+  const deleteuser = async (id ,name) => {
+    const isconfirm = window.confirm(`Are you sure you want to delete this user " ${name} "` );
+    if(isconfirm){
+      await axios.delete(`http://localhost:8000/users/${id}`).then((res)=>{
+        setUser(res.data);
+        setSearch(res.data);
+      })
+    }
+  }
+
+  useEffect(()=>{
+    getAlluser();
+  },[]);
+
+  return (
+    <div className="container">
+      <h3>CRUD Application with React.js Frontend and Node.js Backend</h3>
+      <div className="input-search">
+        <input type="search" placeholder='Search Here ' onChange={changes}/>
+        <button className='btn green' onClick={AddRecordbtn}>Add Record</button>
+      </div>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>City</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {search && search.map((us , index)=>{
+            return(
+              <tr key = {us.id}>
+            <td>{index+1}</td>
+            <td>{us.name}</td>
+            <td>{us.age}</td>
+            <td>{us.city}</td>
+            <td><button className='btn green' onClick={()=>updateDetail(us)}>Edit</button></td>
+            <td><button className='btn red' onClick={()=>deleteuser(us.id , us.name)}>Delete</button></td>
+          </tr>
+            )
+          })}
+          
+        </tbody>
+      </table>
+      {model && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className='close' onClick={close}>
+              &times;
+            </span>
+            <h2>{add.id ? 'Update Record' : 'Add Record'}</h2>
+            <div className="input-group">
+              <label htmlFor="name" > Full Name</label>
+              <input type="text" name ='name' id='name' value={add.name} onChange={handleData} />
+            </div>
+            <div className="input-group">
+              <label htmlFor="age"> Age</label>
+              <input type="number" name ='age' id='age' value={add.age} onChange={handleData} />
+            </div>
+            <div className="input-group">
+              <label htmlFor="city"> City</label>
+              <input type="text" name ='city' id='city' value={add.city} onChange={handleData} />
+            </div>
+            <button className='btn green'onClick={adduser} >{add.id ?"Update User":"Add User"}</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const users = require("./sample.json")
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+
+const fs = require("fs");
+const port = 8000;  
+//display All users
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods :["GET","POST","PATCH","DELETE"],
+}));
+app.get("/users", (req,res)=>{
+return res.json(users);
+});
+//Add New User
+
+app.post("/users" , (req,res)=>{
+    const {name,age , city} = req.body;
+    //console.log("req: ",req.body);
+   
+    let id = Date.now();
+    users.push({id,name,age,city});
+
+    fs.writeFile("./sample.json" , JSON.stringify(users),(err , data)=>{
+        if(err){
+           return window.log(err);
+            
+        }
+        return res.json({message:"user detail added  successfully"});
+    });
+
+    
+})
+
+//Update User
+
+app.patch("/users/:id" , (req,res)=>{
+    let id = Number(req.params.id);
+
+    const {name,age , city} = req.body;
+    //console.log("req: ",req.body);
+   
+    
+    let index = users.findIndex((us)=>us.id == id);
+    users.splice(index , 1,{...req.body});
+
+    fs.writeFile("./sample.json" , JSON.stringify(users),(err , data)=>{
+        if(err){
+           return window.log(err);
+            
+        }
+        return res.json({message:"user detail Updated successfully"});
+    });
+
+    
+})
+
+
+
+//delete user detail
+app.delete("/users/:id",(req,res)=>{
+    const id = Number( req.params.id);
+    const filtereduser = users.filter((us) =>us.id !== id);
+    fs.writeFile("./sample.json" , JSON.stringify(filtereduser),(err , data)=>{
+        if(err){
+           return window.log(err);
+            
+        }
+        return res.json(filtereduser);
+    });
+
+})
+app.listen(port , (err)=>{
+    console.log(`App is running in port ${port}`);
+})
